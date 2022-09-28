a97a2ad23426b528c87dcc71e1b248bde184d606
Joris van Velzen <100634009+Ardjoris@users.noreply.github.com>
2022-09-28 13:14:40 +0200

    almost ready
    I only want to add a (test) button

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 // put line monitor_speed = 115200   in platformio.ini
 
+
 #include "Arduino.h"
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);       // Initialize the LED_BUILTIN pin as an output
- // pinMode(BUTTON_PIN, INPUT_PULLUP);  // Initialize button pin with built-in pullup.
-  digitalWrite(LED_BUILTIN, HIGH);    // Ensure LED is off
-  Serial.begin(115200);               // Set comm rate to 115200
-
- 
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);    // Initialize the LED_BUILTIN pin as an output                                   
+  digitalWrite(LED_BUILTIN, HIGH); // Ensure LED is off
+  Serial.begin(115200);            // Set comm rate to 115200
 }
 
-void loop() {
-  
-    digitalWrite(LED_BUILTIN, HIGH);    // Turn LED Off 
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-  }
+void loop()
+{
+
+  digitalWrite(LED_BUILTIN, HIGH); // Turn LED Off
+  Serial.println("LED High ");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED Low");
+  delay(1000);
+}
